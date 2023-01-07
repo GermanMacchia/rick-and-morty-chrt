2a234940486e0b1c97ab1b5b14fa3036830ecc34
German O. Macchia <germanmacchia@gmail.com>
2023-01-07 01:11:08 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# TP1 =>  BreakingBad
+
+Vue.js intermedio Lleva tus bases al siguiente nivel 
+Fernando Herrera - Udemy
---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+import NavBar from './shared/components/NavBar.vue'
+</script>
+
+<template>
+    <div class="wrapper">
+        <header>
+            <NavBar title="BreakingBad" />
+        </header>
+        <main>
+            <RouterView />
+        </main>
+    </div>
+</template>
+
+<style scoped>
+.wrapper {
+    display: flex;
+    flex-direction: column;
+}
+</style>
